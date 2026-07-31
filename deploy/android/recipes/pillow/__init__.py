961570d4305a39f969546fa9f0c345667e9e0528
Koishi-Marisa <Koishi-Marisa@users.noreply.github.com>
2026-07-31 03:33:44 +0000

    fix(android): use pypi.python.org sdist URL for Pillow

--- deploy/android/recipes/pillow/__init__.py
+++ deploy/android/recipes/pillow/__init__.py
@@ -10,7 +10,7 @@ class PillowRecipe(PyProjectRecipe):
     """
 
     version = "11.3.0"
-    url = "https://files.pythonhosted.org/packages/source/P/Pillow/Pillow-{version}.tar.gz"
+    url = "https://pypi.python.org/packages/source/p/pillow/pillow-{version}.tar.gz"
     hostpython_prerequisites = ["setuptools>=77"]
 
     def build_arch(self, arch):
